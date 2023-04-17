f5e42fe453524f4c1febb327e3b7f7a8548d6731
Jiseok Kim <86154028+p1ants@users.noreply.github.com>
2023-04-17 18:53:20 +0900

    메인페이지, about 페이지 라우팅 확인.  +스타일드 컴포넌트 파일 생성

---
+++ portfolio/src/pages/about.tsx
@@ -0,0 +1,19 @@
+// // pages/about.tsx
+// import React from 'react';
+
+// const AboutPage: React.FC = () => {
+//   return (
+//     <div>
+//       <h1>안녕하세요, About 페이지입니다!</h1>
+//       {/* About 페이지의 다른 컴포넌트와 내용을 이곳에 추가하세요 */}
+//     </div>
+//   );
+// };
+
+// export default AboutPage;
+
+import React from 'react';
+
+export default function aboutPage() {
+  return <div>sss</div>;
+}
--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -1,5 +1,21 @@
+// pages/index.tsx
 import React from 'react';
+import { useRouter } from 'next/router';
 
-export default function index() {
-  return <div>main페이지</div>;
-}
+const MainPage: React.FC = () => {
+  const router = useRouter();
+
+  const goToAboutPage = async () => {
+    await router.push('/about');
+  };
+
+  return (
+    <div>
+      <h1>안녕하세요, 메인 페이지입니다!</h1>
+      {/* 메인 페이지의 다른 컴포넌트와 내용을 이곳에 추가하세요 */}
+      <button onClick={goToAboutPage}>About 페이지로 이동</button>
+    </div>
+  );
+};
+
+export default MainPage;
