7be30243db960b8a5e9b5227345ffdf08d6f865b
Jiseok Kim <86154028+p1ants@users.noreply.github.com>
2023-04-24 18:13:46 +0900

    엄마 미안 나는 틀렸어..

--- portfolio/src/components/Projects/Projects.styles.ts
+++ portfolio/src/components/Projects/Projects.styles.ts
@@ -17,6 +17,30 @@ export const ProjectCard = styled.div`
     box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
     transform: translateY(-10px);
   }
+  /* 애니메이션 추가 */
+  opacity: 0;
+  transform: translateY(20px);
+  animation: fadeIn 1s forwards;
+  animation-delay: calc(var(--animation-order) * 300ms);
+
+  &:nth-child(2) {
+    --animation-order: 1;
+  }
+
+  &:nth-child(3) {
+    --animation-order: 2;
+  }
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+      transform: translateY(20px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
 `;
 
 export const ProjectImage = styled.div`
